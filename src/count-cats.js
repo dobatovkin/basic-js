f675318ec215d71b906fa909d2484fc3f88f03d5
Yahor Dabatoukin <yahor.dabatoukin@mapbox.com>
2023-12-23 23:12:24 +0300

    resove count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  let catCounter = 0;
+  matrix.forEach(row => {
+    row.forEach(element => {
+      if (element === '^^'){
+        catCounter++;
+      }
+    });
+  });
+  return catCounter;
 }
 
 module.exports = {
